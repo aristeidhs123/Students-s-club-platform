32f2a748ed83e30695592b011f00dab62d2db7ca
Maria Tzavara <m.tzavara05@gmail.com>
2026-05-25 16:56:45 +0300

    Delete controllers/internalScheduleRoutes.js

--- controllers/internalScheduleRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createInternalSchedule } from "../controllers/internalScheduleController.js";
-
-const router = express.Router();
-
-router.post("/", createInternalSchedule);
-
-export default router;
