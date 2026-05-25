572e95c63594bf78c5ef93609267fb49a4eef2f3
Maria Tzavara <m.tzavara05@gmail.com>
2026-05-25 16:59:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import db from "./config/db.js";
 import chatbotRoutes from "./routes/chatbotRoutes.js";
+import internalScheduleRoutes from "./routes/internalScheduleRoutes.js";
+import internalActivityRoutes from "./routes/internalActivityRoutes.js";
+import internalPermissionRoutes from "./routes/internalPermissionRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/chatbot", chatbotRoutes);
+
+app.use("/api/internal-schedules", internalScheduleRoutes);
+
+app.use("/api/internal-activities", internalActivityRoutes);
+
+app.use("/api/internal-permissions", internalPermissionRoutes);
 
 app.get("/", (req, res) => {
     res.send("Backend works!");
